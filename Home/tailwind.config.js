8e590a92ecb71b03c6114bbfa98a4d3f925b4790
abujoshu <abujoshua129@yahoo.com>
2024-04-18 10:52:40 +0100

    a

---
+++ Home/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      
+      animation: {
+        'bounce-slow': 'bounce 4s linear infinite',
+  },
+  plugins: [],
+}
+}
+}
